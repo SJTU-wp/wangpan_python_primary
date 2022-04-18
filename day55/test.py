f5a3dcb8c87ca10640f000a85c83d4f4357b6969
wp <807304199@qq.com>
2022-04-18 23:59:40 +0800

    wp-da-3

---
+++ day55/test.py
@@ -0,0 +1,18 @@
+# author: wp
+# 2022年04月18日 13:21
+
+print("""    00011001\n\nprint(\n"Life is short,\n\tyou need Python."\n)""")
+print(
+    """
+    Life is short,
+    you need Python.
+    """
+)
+print(
+    "Life is short, \nyou need Python."
+)
+
+print(
+    "Life is short, "
+    "you need Python."
+)
